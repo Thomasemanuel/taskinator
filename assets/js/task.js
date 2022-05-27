d0ac74ac4cd7d8be928ab06ef70d294316fdb2e8
Tom Emanuel <104587898+Thomasemanuel@users.noreply.github.com>
2022-05-27 11:09:10 -0400

    submit button adds a new list

--- assets/js/task.js
+++ assets/js/task.js
@@ -1,20 +1,24 @@
 
 // the html is accessed by using document giving us the button 
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var formEl = document.querySelector("#task-form");
 
-// var is the section element id is tasks-to-do
+// var is the section element, id is tasks-to-do
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+
 //after clicked this will run
-var createTaskHandler = function() {
-//add new li item with class name task item, text "this is new task"
-  var listItemEl = document.createElement("ul");
-  listItemEl.className = "task-form";
-  listItemEl.textContent = "This is a new task.";
-  //add to the end of the li items
-  tasksToDoEl.appendChild(listItemEl);
+var createTaskHandler = function(event) {
+    //prevent default text 
+    event.preventDefault();
+    //add new li item with class name task item, text "this is new task"
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    //add to the end of the li items
+    tasksToDoEl.appendChild(listItemEl);
 };
 //when clicked run createTaskhandler
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener('submit',createTaskHandler);
+
+
 
 
